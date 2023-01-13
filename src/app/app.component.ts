f37ec722d22c3328168692630693ab27486f85c4
Matthew Haugen <mhaugen@haugenapplications.com>
2023-01-12 22:12:45 -0800

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { map, Subject, takeUntil } from "rxjs";
+import { IProductInformation } from "./types";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'jnjsmartformpoc';
+export class AppComponent implements OnDestroy {
+  title = 'JNJ Smart Form POC';
+
+  currentStepIndex = 0;
+  problemDetailsReady = false;
+  reviewReady = false;
+
+  product: IProductInformation = {
+    brand: "",
+    lotNumber: ""
+  }
+  //productFormGroup: Formg
+
+  private readonly destroy$ = new Subject<void>();
+
+  constructor(activedRoute: ActivatedRoute) {
+    activedRoute.queryParamMap
+      .pipe(
+        map((c): IProductInformation => ({
+          brand: c.get("product.brand") ?? "",
+          lotNumber: c.get("product.lot") ?? ""
+        })),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(c => {
+        this.product = c;
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+  }
 }
---
+++ src/app/types.d.ts
@@ -0,0 +1,5 @@
+
+export interface IProductInformation {
+    brand: string;
+    lotNumber: string;
+}
