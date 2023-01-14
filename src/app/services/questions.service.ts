b6ab949e2ac907df459c2fe2658da99a8aef4d10
Goutham Velicheti <GVeliche@its.jnj.com>
2023-01-13 16:41:18 -0800

    Add couple of for more questions

--- src/app/services/questions.service.ts
+++ src/app/services/questions.service.ts
@@ -48,13 +48,166 @@ const allQuestions: Question[] = [{
     questionText: "How did you learn how to use the injector?",
     options: [
       {
-        name: "My healthcare provider showed me"
+        name: "My healthcare provider showed me",
+        nextQuestionId: "field_sample_available",
       },
       {
         name: "I read the instructions"
       },
       {
         name: "I watched videos online"
+      }
+    ]
+  },
+  {
+    id: "field_sample_available",
+    type: "multiple-choice",
+    required: true,
+    questionText: "Field sample available?",
+    options: [
+      {
+        name: "Initiate field sample return",
+        nextQuestionId: "first_use_of_product"
+      }
+    ]
+  },
+  {
+    id: "first_use_of_product",
+    type: "multiple-choice",
+    required: true,
+    questionText: "Was this the first use of the product by the patient?",
+    options: [
+      {
+        name: "Yes",
+        nextQuestionId: "first_administration_HCP"
+      },
+      {
+        name: "No",
+        nextQuestionId: "first_administration_HCP"
+      }
+    ]
+  },
+  {
+    id: "first_administration_HCP",
+    type: "multiple-choice",
+    required: true,
+    questionText: "Was this the first administration given to a patient by this HCP?",
+    options: [
+      {
+        name: "Yes",
+        nextQuestionId: "product_Inspection"
+      },
+      {
+        name: "No",
+        nextQuestionId: "product_Inspection"
+      }
+    ]
+  },
+  {
+    id: "product_Inspection",
+    type: "multiple-choice",
+    required: true,
+    questionText: "Are Product Inspected",
+    options: [
+      {
+        name: "Missing Components / Excess Components",
+        nextQuestionId: "missing_components"
+      },
+      {
+        name: "Damaged Components",
+        nextQuestionId: "damaged_components"
+      }
+    ]
+  },
+  {
+    id: "missing_components",
+    type: "multiple-choice",
+    required: true,
+    questionText: "Upon opening the product, were any components missing from the package?",
+    options: [
+      {
+        name: "Pre-filled Syringe",
+        nextQuestionId: "extra_components"
+      },
+      {
+        name: "Syringe Backstop",
+        nextQuestionId: "extra_components"
+      },
+      {
+        name: "Plunger Rod",
+        nextQuestionId: "extra_components"
+      },
+      {
+        name: "Tip Cap",
+        nextQuestionId: "extra_components"
+      }
+    ]
+  },
+  {
+    id: "extra_components",
+    type: "multiple-choice",
+    required: true,
+    questionText: "Upon opening the product, were any extra components received in the package?",
+    options: [
+      {
+        name: "Pre-filled Syringe",
+        nextQuestionId: "blister_seals"
+      },
+      {
+        name: "Syringe Backstop",
+        nextQuestionId: "blister_seals"
+      },
+      {
+        name: "Plunger Rod",
+        nextQuestionId: "blister_seals"
+      },
+      {
+        name: "Tip Cap",
+        nextQuestionId: "blister_seals"
+      }
+    ]
+  },
+  {
+    id: "blister_seals",
+    type: "multiple-choice",
+    required: true,
+    questionText: "Upon opening the product, were any of the blister seals already opened?",
+    options: [
+      {
+        name: "The kit tray containing the pre-filled syringe and needle blisters (sterile_breached)",
+        nextQuestionId: "sterile_breached"
+      },
+      {
+        name: " 1 ½” Thin Wall Needle (yellow) blister (sterile_breached)",
+        nextQuestionId: "sterile_breached"
+      }
+    ]
+  },
+  {
+    id: "sterile_breached",
+    type: "multiple-choice",
+    required: true,
+    questionText: "Was a sterile barrier breached",
+    options: [
+      {
+        name: "Customer unable/unwilling to answer",
+        
+      }
+    ]
+  },
+  {
+    id: "damaged_components",
+    type: "multiple-choice",
+    required: true,
+    questionText: "Upon opening the product, were any components damaged?",
+    options: [
+      {
+        name: "IFU Leaflet",
+        nextQuestionId: "blister_seals"
+      },
+      {
+        name: "Patient Insert",
+        nextQuestionId: "blister_seals"
       }
     ]
   }
